0918ed3cb0f0f6d250d0e51d35f2792c8192234e
Your Name <you@example.com>
2024-08-05 05:13:35 +0100

    login with token

--- users/views.py
+++ users/views.py
@@ -28,39 +28,46 @@ class LoginView(APIView):
         if not user.check_password(password):
             raise AuthenticationFailed('Incorrect password!')
 
-        # payload = {
-        #     'id': user.id,
-        #     'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-        #     'iat': datetime.datetime.utcnow()
-        # }
-        #
-        # token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
-        #
-        # response = Response()
-        #
-        # response.set_cookie(key='jwt', value=token, httponly=True)
-        # response.data = {
-        #     'jwt': token
-        # }
-        return Response({'message': 'You are logged in!'})
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+        return response
 
 
 class UserView(APIView):
 
     def get(self, request):
+
         token = request.COOKIES.get('jwt')
 
         if not token:
             raise AuthenticationFailed('Unauthenticated!')
 
         try:
-            payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
         except jwt.ExpiredSignatureError:
             raise AuthenticationFailed('Unauthenticated!')
 
         user = User.objects.filter(id=payload['id']).first()
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+        # return Response({
+        #     'id': user.id,
+        #     'email': user.email,
+        #     'name': user.name
+        # })
 
 
 class LogoutView(APIView):
